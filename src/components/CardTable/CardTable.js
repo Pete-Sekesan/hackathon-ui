29f64e2da47459cc953995812e9200a6b0e6024a
Andrew <ramettaandrewj@gmail.com>
2021-08-03 13:04:55 -0400

    add betting

--- src/components/CardTable/CardTable.js
+++ src/components/CardTable/CardTable.js
@@ -1,3 +1,4 @@
+import { findAllByDisplayValue } from "@testing-library/react";
 import React, { useState } from "react";
 import AuthAPIService from "../../services/auth-api-service";
 
@@ -8,6 +9,9 @@ function CardTable(props) {
   const [cards, setCards] = useState({});
   const [playerCard, setPlayerCard] = useState({});
   const [dealerCard, setDealerCard] = useState({});
+  const [bank, playerBank] = useState(500);
+  const [bet, setBet] = useState(0);
+  const [placeBet, setPlaceBet] = useState(false);
 
   const shuffleCards = () => {
     AuthAPIService.shuffleCards()
@@ -15,6 +19,9 @@ function CardTable(props) {
         console.log(deck.deck_id);
         setDeckId(deck.deck_id);
         setDeckAmount(deck.remaining);
+        setPlayerCard(null);
+        setDealerCard(null);
+        setBet("");
         console.log(deck);
       })
       .catch((err) => {
@@ -35,10 +42,52 @@ function CardTable(props) {
       });
   };
 
+  const betTen = () => {
+    setBet(bet + 10);
+  };
+  const betFifty = () => {
+    setBet(bet + 50);
+  };
+  const betHundred = () => {
+    setBet(bet + 100);
+  };
+
   return (
     <div>
-      <button onClick={shuffleCards}>Shuffle Cards</button>
-      <button onClick={dealCards}>Deal Cards</button>
+      <button onClick={shuffleCards}>Deal Cards</button>
+      <button>Place Bet</button>
+      {placeBet === false ? (
+        <>
+          <button onClick={betTen}>$10</button>
+          <button onClick={betFifty}>$50</button>
+          <button onClick={betHundred}>$100</button>
+        </>
+      ) : (
+        <button onClick={dealCards}>Reveal Cards</button>
+      )}
+
+      {playerCard !== null ? (
+        <img src={playerCard.image} width="200" heigh="400" />
+      ) : (
+        <img
+          src="https://cdn.shopify.com/s/files/1/0200/7616/products/playing-cards-bicycle-rider-back-1_1024x1024.png?v=1535755695"
+          width="200"
+          heigh="400"
+        />
+      )}
+      {dealerCard !== null ? (
+        <img src={dealerCard.image} width="200" heigh="400" />
+      ) : (
+        <img
+          src="https://cdn.shopify.com/s/files/1/0200/7616/products/playing-cards-bicycle-rider-back-1_1024x1024.png?v=1535755695"
+          width="200"
+          heigh="400"
+        />
+      )}
+      {bet === 0 ? <p>Current Bet: $0</p> : <p>Current Bet: ${bet}</p>}
+      <p>Current Bet Test: ${bet}</p>
+
+      {bank}
     </div>
   );
 }
