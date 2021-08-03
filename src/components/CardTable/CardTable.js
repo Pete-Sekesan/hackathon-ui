8023778c2bb6affaaa7aa949f2c6fd68768c3622
Andrew <ramettaandrewj@gmail.com>
2021-08-03 10:23:21 -0400

    added deal button

--- src/components/CardTable/CardTable.js
+++ src/components/CardTable/CardTable.js
@@ -1,13 +1,39 @@
 import React, { useState } from "react";
 import AuthAPIService from "../../services/auth-api-service";
 
-function CardTable() {
-  const { deal, setDeal } = useState([]);
+function CardTable(props) {
+  const [deal, setDeal] = useState([]);
+  const [deckId, setDeckId] = useState("");
+  const [deckAmount, setDeckAmount] = useState("");
 
-  const dealCards = () => {};
+  const shuffleCards = () => {
+    AuthAPIService.dealCards()
+      .then((deck) => {
+        console.log(deck.deck_id);
+        setDeckId(deck.deck_id);
+        setDeckAmount(deck.remaining);
+        console.log(deck);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const dealCards = () => {
+    AuthAPIService.dealCards()
+      .then((deck) => {
+        console.log(deck.deck_id);
+        setDeckId(deck.deck_id);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <div>
-      <button>Draw</button>
+      <button onClick={shuffleCards}>Shuffle Cards</button>
+      <button onClick={dealCards}>Deal Cards</button>
     </div>
   );
 }
