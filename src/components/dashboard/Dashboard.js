a19885333ee5f69e5441aaec6da096726b197c5c
Peter Sekesan <psekesan@gmail.com>
2021-08-04 14:18:38 -0400

    start of leaderboard component

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { Component, Fragment, useState, useContext } from "react";
 import Wallet from '../Wallet/Wallet';
+import LeaderBoard from '../LeaderBoard/LeaderBoard';
 
 
 
 function Dashboard() {
-    return (
-      <Wallet/>
+  return (
+    <Fragment>
+      <div>
+        <Wallet />
+      </div>
+      <div>
+        <LeaderBoard />
+        </div>
+      </Fragment>
+    
     )
 }
 
